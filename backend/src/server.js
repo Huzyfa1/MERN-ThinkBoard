a628d04a26392c5268bdea40c8715c5dcc463264
Huzyfa1 <huzaifarehman164@gmail.com>
2025-07-20 13:13:24 -0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
+
+//Middleware
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
+app.use(express.json()); //this middleware parse JSON bodies: req.body
+app.use(rateLimiter);
+
+//Our simple custom middleware
+// app.use((req, res, next)=>{
+//     console.log (`Req method is ${req.method} and Req URL is ${req.url}`)
+//     next();
+// })
+
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (res, req) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+});
